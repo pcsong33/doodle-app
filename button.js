bcf3cb8ce2c463c24b76973a259ed400448967a2
Patrick <pcsong33@gmail.com>
2022-09-21 11:27:49 -0400

    add comments

--- button.js
+++ button.js
@@ -1,3 +1,4 @@
+// if a button has been clicked, enable submit button
 function buttonChange() {
     document.getElementById("submit").setAttribute("href", "submitted.html");;
     document.getElementById("button").style.backgroundColor = "green";
