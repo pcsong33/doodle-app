c85eb741eca7ae4a248e8961506a835140957ce8
Patrick <pcsong33@gmail.com>
2022-09-21 10:06:45 -0400

    finish doodle poll

---
+++ button.js
@@ -0,0 +1,4 @@
+function buttonChange() {
+    document.getElementById("submit").setAttribute("href", "submitted.html");;
+    document.getElementById("button").style.backgroundColor = "green";
+  }
