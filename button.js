c12ce6f9e72c1a3d47548c8c6cfbb853097b3cfd
Kevin Huang <kev.y.huang@gmail.com>
2022-09-22 14:49:26 -0400

    Updated design for selecting availability

--- button.js
+++ button.js
@@ -1,5 +1,24 @@
-// if a button has been clicked, enable submit button
-function buttonChange() {
+// Toggles between the available, maybe, and not available glyph symboles
+function tristate(id) {
+  var icon = document.getElementById(id);
+  switch (icon.className) {
+    case "glyphicon glyphicon-ok-sign":
+      icon.classList.toggle("maybe");
+      break;
+    case "glyphicon glyphicon-ok-sign maybe":
+      icon.classList.toggle("glyphicon-ok-sign");
+      icon.classList.toggle("maybe");
+      icon.classList.toggle("glyphicon-remove");
+      break;
+    case "glyphicon glyphicon-remove":
+      icon.classList.toggle("glyphicon-remove");
+      icon.classList.toggle("glyphicon-ok-sign");
+      break;
+    default:
+      // display the current value if it's unexpected
+      alert(control.value);
+  }
+  // Enables submit button after the users has toggled a setting
   document.getElementById("submit").setAttribute("href", "submitted.html");
   document.getElementById("button").style.backgroundColor = "green";
 }
